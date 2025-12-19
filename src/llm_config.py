4962f36bfca187acb3aeec62157c24e08c045ffb
DavidGCalles <davidgcalles91@gmail.com>
2025-12-19 10:50:49 +0100

    Hm... this is fucked up, i think its a langchain problem

--- src/llm_config.py
+++ src/llm_config.py
@@ -1,14 +1,9 @@
-'''
-Docstring for src.llm_config
-'''
 from langchain_openai import ChatOpenAI
 
-# Apuntamos al servicio 'litellm' definido en el docker-compose
-# La key puede ser cualquiera, LiteLLM usa la real del .env
 llm = ChatOpenAI(
     model="crewai-proxy", 
-    openai_api_base="http://litellm:4000",
-    openai_api_key="sk-fake-key", 
+    base_url="http://litellm:4000",
+    api_key="sk-fake-key",  
     temperature=0.7,
     max_retries=1
 )
