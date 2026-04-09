90e342aff0f61357a7c49b6ff1a8cac51d4c4ea5
David G. Calles <davidgcalles91@gmail.com>
2026-04-09 14:46:18 +0200

    Refer #153
    Edge profile is working properly

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,3 @@
 # Exponemos las utilidades para facilitar imports
-from .session_manager import SessionManager
 from .radar import available_models
 from .vision_parser import render_document_to_images, vision_extract_text, vision_extract_text_sync
